4c66d93b804617a1837ff2b2794104d8dbc43e16
thebvrtosz <bartosz.gejgal@gmail.com>
2022-12-01 16:06:33 +0100

    making corrections to errors stated in commit comments

--- d01.py
+++ d01.py
@@ -1,21 +1,23 @@
-def get_kcal_per_elve(input):
-    kcal_per_elve = []
-    elve_kcal = 0
-    for kcal in input.split('\n'):
-        if kcal == '':
-            kcal_per_elve.append(elve_kcal)
-            elve_kcal = 0
+def get_kcal_per_elf(input):
+    kcal_per_elf = []
+    elf_kcal = 0
+    for kcal in input.splitlines():
+        if not kcal:
+            kcal_per_elf.append(elf_kcal)
+            elf_kcal = 0
             continue
-        elve_kcal += int(kcal)
-    kcal_per_elve.sort(reverse=True)
-    return kcal_per_elve
+        elf_kcal += int(kcal)
+    kcal_per_elf.sort(reverse=True)
+    return kcal_per_elf
+
+
 
 
 if __name__ == '__main__':
     with open('inputs/d01-input.txt', 'r') as f:
         cnt = f.read()
-    kcal_per_elve = get_kcal_per_elve(cnt)
+    kcal_per_elf = get_kcal_per_elf(cnt)
     #part1
-    print(kcal_per_elve[0])
+    print(kcal_per_elf[0])
     #part2
-    print(sum(kcal_per_elve[:3]))
+    print(sum(kcal_per_elf[:3]))
